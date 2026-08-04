408a5854917948840d6032c0b5fba6f40a39cbc9
ahmed-rakib <rakib.swe02@gmail.com>
2026-08-04 17:20:35 +0600

    Update Lesson 01

--- 01_Hello_fastAPI/main.py
+++ 01_Hello_fastAPI/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def home():
     return {"message": "Hello FastAPI"}
+
+
+@app.get("/about")
+async def about():
+    return {"project":"This is fastAPI project"}
